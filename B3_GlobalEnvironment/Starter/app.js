e68483b554c0e0305c9e95fa1cc1c7fe57fbfcfb
Fernando <ffmp777@yahoo.com>
2016-12-06 05:48:52 -0700

    hoisting

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -1,5 +1,11 @@
+//b();            //functions are set up in their entirety. "Hoisting"
+//console.log(a); //a will = undefined. Because all var are initially set to 'undefined' by the JS engine.
+
 var a = "Hello World!";
 
 function b() {
-
+    console.log('Called b!');
 }
+
+//b();
+//console.log(a); //a will = "Hello World!"
