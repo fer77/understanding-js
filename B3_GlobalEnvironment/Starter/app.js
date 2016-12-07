b547885de09a69fc4407ef3b524380a17f9b91b6
Fernando <ffmp777@yahoo.com>
2016-12-06 20:11:53 -0700

    undefined

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -1,11 +1,29 @@
+// HOISTING //
+
 //b();            //functions are set up in their entirety. "Hoisting"
 //console.log(a); //a will = undefined. Because all var are initially set to 'undefined' by the JS engine.
 
-var a = "Hello World!";
+//var a = "Hello World!";
 
-function b() {
-    console.log('Called b!');
-}
+//function b() {
+//    console.log('Called b!');
+//}
 
 //b();
 //console.log(a); //a will = "Hello World!"
+
+// UNDEFINED //
+
+//var a; //This will throw an undefined.
+
+var a = "I am defined!";
+
+//a = undefined; //Never do this.
+
+console.log(a); //Without var a this would throw a "...referenceError: a is not defined"
+
+if (a === undefined) {
+    console.log('a is undefined! :-(');
+} else {
+    console.log('a has been defined! :-)');
+}
