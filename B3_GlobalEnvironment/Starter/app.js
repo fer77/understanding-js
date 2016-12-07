8fc9e64b80b113d112a175b07171afe10089fb22
Fernando <ffmp777@yahoo.com>
2016-12-07 05:34:58 -0700

    execution

--- B3_GlobalEnvironment/Starter/app.js
+++ B3_GlobalEnvironment/Starter/app.js
@@ -16,14 +16,28 @@
 
 //var a; //This will throw an undefined.
 
-var a = "I am defined!";
+//var a = "I am defined!";
 
 //a = undefined; //Never do this.
 
-console.log(a); //Without var a this would throw a "...referenceError: a is not defined"
+//console.log(a); //Without var a this would throw a "...referenceError: a is not defined"
 
-if (a === undefined) {
-    console.log('a is undefined! :-(');
-} else {
-    console.log('a has been defined! :-)');
+//if (a === undefined) {
+//    console.log('a is undefined! :-(');
+//} else {
+//    console.log('a has been defined! :-)');
+//}
+
+// EXECUTION
+
+function b() {
+    console.log('Called b!');
 }
+
+b(); // Outputs 'Called b!'
+
+console.log(a); // Outputs Undefined, because of the execution context.
+
+var a = "Hello World!";
+
+console.log(a); // Outputs "Hello World!"
