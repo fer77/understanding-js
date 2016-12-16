480c39d0d36e34133aadf95095a85e9e575268f8
Fernando <ffmp777@yahoo.com>
2016-12-15 22:57:38 -0700

    Default values

--- app.js
+++ app.js
@@ -210,20 +210,49 @@
 //Boolean(""); //* returns false.
 //Boolean(0); //* returns false;
 
-var a;
+//var a;
 
-if (a) {
+//if (a) {
     //* nothing will be logged in the console, because there is no value for a.
-    console.log('Something is there.');
-}
+//    console.log('Something is there.');
+//}
 
 //* if we give 'a' a value
 
-a = 0;
+//a = 0;
 
-if (a || a === 0) {
+//if (a || a === 0) {
     //* this will log 'Something is there' because === goes left to right and converts to false || true and this always returns true.
     //* try: false || true  &  true || false, the console will return 'true'.
-    console.log('Something is there.');
+//    console.log('Something is there.');
+//}
+
+// DEFAULT VALUES
+
+//function greet(name) {
+//    console.log(name);
+//    console.log('Hello ' + name);
+//}
+
+//greet('Fernando');
+
+
+//agreet();
+//* The + operator will coerse undefined to a string and return 'Hello undefined'.
+
+//* in the console:
+//0 || 1
+//* returns 1 It returns whatever value coerses to true.
+//undefined || 'hello'
+//* returns "hello" because it is coersed to true.  That is the || special behavior, it returns whatever value coerses to true first.
+//Boolean("hello")
+//* returns true
+
+//* Setting a default value:
+function greet(name) {
+    name = name || '<your name here>'; //* Order of operation will run || first and then the = sign. If name is true it will return true otherwise it'll return the default.
+    console.log('Hello ' + name);
 }
 
+greet('Fernando');
+greet();
