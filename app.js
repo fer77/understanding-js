738d27643f19db57e91738996efa43d41ab88cfa
Fernando <ffmp777@yahoo.com>
2016-12-17 16:20:54 -0700

    Faking namespaces

--- app.js
+++ app.js
@@ -287,29 +287,61 @@
 
 //OBJECT LITTERALS
 
-var fernando = {
-    firstName: 'Fernando',
-    lastName: 'Fernandez',
-    address: {
-        street: '777 State st.',
-        city: 'Springville',
-        state: 'UT'
-    }
-};
-console.log(fernando);
+//var fernando = {
+//    firstName: 'Fernando',
+//    lastName: 'Fernandez',
+//    address: {
+//        street: '777 State st.',
+//        city: 'Springville',
+//        state: 'UT'
+//    }
+//};
+//console.log(fernando);
 
-function greet(person) {
-        console.log('Hello ' + person.firstName);
-}
+//function greet(person) {
+//        console.log('Hello ' + person.firstName);
+//}
 
-greet(fernando);
+//greet(fernando);
 
 //* Object literal: Is using {} to define name and value pairs, separated by colons.
-greet({
-    firstName: 'Katy',
-    lastName: 'Fernandez'
-});
+//greet({
+//    firstName: 'Katy',
+//    lastName: 'Fernandez'
+//});
 
-fernando.address2 = {
-    company: 'Workity work work'
-}
+//fernando.address2 = {
+//    company: 'Workity work work'
+//}
+
+// NAMESPACES
+
+var greet = 'Hello';
+var greet = 'Hola';
+
+console.log(greet); //* Returns 'Hola' (execution context).
+
+//* We can prevent this collision by creating an object that will be our container:
+
+var english = {};
+var spanish = {};
+
+//* Now our variables will not collide with ea. other.
+english.greet = 'Hello';
+spanish.greet = 'Hola';
+
+//console.log(english);
+
+//* To create a new object to hold difrent types of greetings we can not do this:
+// english.greetings.greet = 'Hello'; //* This will return undefined, because the greetings object does not yet exist inside of english.
+//* First do this:
+//english.greetings = {};
+//* Then:
+//english.greetings.greet = 'Hello';
+//* Or:
+var english = {
+    greetings: {
+        greet: 'Hello'
+    }
+};
+console.log(english);
