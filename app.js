30c6f1329a63f4b2f2dc969069c8af1fbe433989
Fernando <ffmp777@yahoo.com>
2016-12-17 14:18:10 -0700

    Objects

--- app.js
+++ app.js
@@ -259,5 +259,30 @@
 
 //FRAMEWORKS: DEFAULT VALUES
 
-console.log(libraryName);  //* 'lib2' will be logged, becuase libraryName was attached to the window object first as 'lib1', but
+// console.log(libraryName);  //* 'lib2' will be logged, becuase libraryName was attached to the window object first as 'lib1', but
                             //* overwritten by lib2.js libraryName declaration.
+// OBJECTS
+var person = new Object();
+
+person['firstName'] = 'Fernando';
+person['lastName'] = 'Fernandez';
+
+var firstNameProperty = 'firstName';
+
+console.log(person);
+
+console.log(person[firstNameProperty]);
+
+console.log(person.firstName);
+console.log(person.lastName);
+
+person.address = new Object();
+person.address.street = '777 State st.';
+person.address.city = 'Springville';
+person.address.state = 'UT';
+
+console.log(person.address.street);
+console.log(person.address.city);
+console.log(person['address']['state']);
+
+
