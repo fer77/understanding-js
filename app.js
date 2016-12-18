954f31711394702cb4a335ff43308eb30eba3d6b
Fernando <ffmp777@yahoo.com>
2016-12-18 13:14:59 -0700

    JSON

--- app.js
+++ app.js
@@ -316,32 +316,54 @@
 
 // NAMESPACES
 
-var greet = 'Hello';
-var greet = 'Hola';
+//var greet = 'Hello';
+//var greet = 'Hola';
 
-console.log(greet); //* Returns 'Hola' (execution context).
+//console.log(greet); //* Returns 'Hola' (execution context).
 
 //* We can prevent this collision by creating an object that will be our container:
 
-var english = {};
-var spanish = {};
+//var english = {};
+//var spanish = {};
 
 //* Now our variables will not collide with ea. other.
-english.greet = 'Hello';
-spanish.greet = 'Hola';
+//english.greet = 'Hello';
+//spanish.greet = 'Hola';
 
 //console.log(english);
 
-//* To create a new object to hold difrent types of greetings we can not do this:
+//* To create a new object to hold diffrent types of greetings we can not do this:
 // english.greetings.greet = 'Hello'; //* This will return undefined, because the greetings object does not yet exist inside of english.
 //* First do this:
 //english.greetings = {};
 //* Then:
 //english.greetings.greet = 'Hello';
 //* Or:
-var english = {
-    greetings: {
-        greet: 'Hello'
-    }
-};
-console.log(english);
+//var english = {
+//    greetings: {
+//        greet: 'Hello'
+//    }
+//};
+//console.log(english);
+
+//JSON
+
+var objectLiteral = {
+    firstName: 'Katy',
+    lastName: 'Fernandez'
+}
+
+//console.log(objectLiteral);
+
+//* json:
+//{
+//    "firstName": "Katy",
+//    "children": true
+//}
+
+//* json is not part of JS, but becuase it is so easy for JS to understand any object can be transformed it to JSON:
+console.log(JSON.stringify(objectLiteral));
+
+//* And for any JSON we can convert it to an object:
+var jsonValue = JSON.parse('{ "firstName": "Katy","children": true }');
+console.log(jsonValue);
