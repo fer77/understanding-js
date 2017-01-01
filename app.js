e69446a23ee5dc9e94805e674ca8a256c2277fb4
Fernando <ffmp777@yahoo.com>
2017-01-01 16:54:23 -0700

    arrays

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+//* ARRAYS
 
+//var arr = new Array();
+
+//* Array literal:
+//* An array can be a collection of anything.
+var arr = [
+    1,
+    false,
+    {
+        name: 'Fernando',
+        address: '111 one St.'
+    },
+    function(someName) {
+        var greeting = 'Hello ';
+        console.log(greeting + someName);
+    },
+    'hello'
+];
+
+console.log(arr);
+
+//* JS' dynamic typing allow for arrays to be referenced and hold functions that can then be invoked:
+arr[3](arr[2].name);
