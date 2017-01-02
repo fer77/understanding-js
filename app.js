66823e96fe30b0c25411506d00ce6117f6f787fd
Fernando <ffmp777@yahoo.com>
2017-01-02 11:50:32 -0700

    No function overload

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
-function greet(firstName, lastName, initials) {
-    //* Default values can be set like this:
-    initials = initials || 'XX';
-
-    //* When no parameters are passed we can have the function do something like this:
-    if (arguments.length === 0) {
-        console.log('Missing parameters');
-        console.log('---------');
-        return;
+//* NO FUNCTION OVERLOAD IN JS
+function greet(firstName, lastName, language) {
+    if (language === 'English') {
+        console.log('Hello ' + firstName + ' ' + lastName);
     }
-
-    console.log(firstName);
-    console.log(lastName);
-    console.log(initials);
-    //* JS' execution context sets up a special keyword that holds all the values passed as arguments in an array-like:
-    console.log(arguments);
-    //* We can access the array-like the same as any other array:
-    console.log('arg 0: ' + arguments[0] + ',',
-                'arg 1: ' + arguments[1] + ',',
-                'arg 2: ' + arguments[2]);
-    console.log('---------');
+    if (language === 'Spanish') {
+        console.log('Hola ' + firstName + ' ' + lastName);
+    }
 }
 
-greet();
-//* Invoking greet without any arguments returns 'undefined', because hoisting gives arguments the value of 'undefined' during the creation phase.
+function greetEnglish(firstName, lastName) {
+    greet(firstName, lastName, 'English');
+}
 
-greet('Fernando');
-greet('Fernando', 'Fernandez');
-greet('Fernando', 'Fernandez', 'FF');
+function greetSpanish(firstName, lastName) {
+    greet(firstName, lastName, 'Spanish');
+}
 
+greetEnglish('Fernando', 'Fernandez');
+greetSpanish('Titan', 'Fernandez');
