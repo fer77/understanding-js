955b81b7d1da11ced7da9a66c8c3b815338c576a
Fernando <ffmp777@yahoo.com>
2017-03-20 21:05:58 -0600

    notes

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 //* Function constructor:
 function Person(firstName, lastName) {
 
-	console.log(this); //* returns the Person object.
+	console.log(this);
 	this.firstName = firstName;
-	this.lastName = lastName;
-	console.log('This function is invoked.'); //* shows we are not calling Person when bob is invoked.
+	this.lastName = lastName; //* This gets put on every single object created, which uses memory space.
+	console.log('This function is invoked.');
 }
 
+//* The prototype of a function is not the prototype of the function.  It is the prototype of any object created by a function being used as a function contstructor.
+Person.prototype.getFullName = function () {
+	return this.firstName + ' ' + this.lastName;
+}
+
+//* When the 'new' keyword is called it creates a new empty object.  It sets the prototype of that empty object to the prototype property of the function that was called.
 var bob = new Person('Bob', 'Belcher');
-console.log(bob); //* the 'new' operator attaches the 'this.' object to bob.
+console.log(bob);
 
 var linda = new Person('Linda', 'Belcher');
-console.log(linda); //* the 'new' operator attaches the 'this.' object to linda.
+console.log(linda);
+
+//* It is better to add methods on the prototype to save in memory space, because 'new' objects would only need one copy of the prototype.
